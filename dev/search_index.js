407066e9b0a50894f7f59f764781814425795777
Documenter.jl <documenter@juliadocs.github.io>
2022-06-23 05:26:15 +0000

    build based on 7ab7e67

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GMRs","category":"page"},{"location":"#GMRs","page":"Home","title":"GMRs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GMRs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GMRs]","category":"page"}]
+}
